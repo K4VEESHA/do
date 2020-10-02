affe8340ec4fa2aafc2fa8a722e5949c334f488b
Kaveesha Thilakarathna <72226897+K4VEESHA@users.noreply.github.com>
2020-10-02 10:34:25 +0530

    Add files via upload

---
+++ four4.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main ()
+
+{
+char x[1];
+int y=0,z=0;
+char n[0];
+char N[0];
+
+do {
+
+  printf("Enter Number : ");
+  scanf("%d",y);
+
+  printf("Do you Want to Continue : ");
+  scanf("%s",&x);
+  z = z + y
+
+
+}
+
+while (x==n || x==N);
+printf("%d",z);
+
+
+
+
+
+}
